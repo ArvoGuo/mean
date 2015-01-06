5396d94fec03bda51178d8b9272919cab621f295
xuyuan923 <xuyuan923@163.com>
2015-01-06 22:24:52 +0800

    up alert

--- public/js/exit.js
+++ public/js/exit.js
@@ -6,18 +6,29 @@ $(function () {
     $('.exit').click(function (e) {
         var target = $(e.target);
         var id = target.data('id');
-        var tr = $('.item-id-'+id);
-        $.ajax({
-            //注意，ajax type可以是delete?其它 HTTP 请求方法，如 PUT 和 DELETE 也可以使用，但仅部分浏览器支持。
-            type: 'POST',
-            url: "/admin/line/my?id=" +id
-        })
-            .done(function (results) {
-                if (results.success === 1) {
-                    if (tr.length > 0) {
-                        tr.remove()
-                    }
-                }
+        var tr = $('.item-id-' + id);
+        swal({
+            title: "您真的要退出吗?",
+            text: "退出后将无法恢复!",
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#DD6B55",
+            confirmButtonText: "退出",
+            closeOnConfirm: false
+        }, function () {
+            $.ajax({
+                //注意，ajax type可以是delete?其它 HTTP 请求方法，如 PUT 和 DELETE 也可以使用，但仅部分浏览器支持。
+                type: 'POST',
+                url: "/admin/line/my?id=" + id
             })
+                .done(function (results) {
+                    if (results.success === 1) {
+                        swal("Success!", "您已成功退出!", "success");
+                        if (tr.length > 0) {
+                            tr.remove()
+                        }
+                    }
+                })
+        })
     })
 })
