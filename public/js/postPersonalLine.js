f615d7e65574e225559a031f9f8a8e1636d293e6
xuyuan923 <xuyuan923@163.com>
2015-01-18 19:27:06 +0800

    up nav active

--- public/js/postPersonalLine.js
+++ public/js/postPersonalLine.js
@@ -3,6 +3,12 @@
  * 获取登录用户所在的业务线
  */
 $(document).ready(function(){
+    function getQueryString(name) {
+        var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+        var r = window.location.search.substr(1).match(reg);
+        if (r != null) return decodeURI(r[2]); return null;
+    }
+    var lineName = getQueryString('lineName');
     $.ajax({
         type: 'POST',
         url: '/admin/line/list/left'
@@ -13,7 +19,11 @@ $(document).ready(function(){
             if(!$lineList) return false;
             if($lineList.length){
                 for(var i=0;i<results.length;i++){
-                    $lineList.prepend('<li><a href="/admin/role/line?lineName='+results[i].name+'&lineId='+results[i]._id+'"><i class="project"></i><span>'+results[i].name+'</span></a></li>')
+                    if(lineName === results[i].name){
+                        $lineList.prepend('<li><a class="active" href="/admin/role/line?lineName='+results[i].name+'&lineId='+results[i]._id+'"><i class="project"></i><span>'+results[i].name+'</span></a></li>')
+                    }else{
+                        $lineList.prepend('<li><a href="/admin/role/line?lineName='+results[i].name+'&lineId='+results[i]._id+'"><i class="project"></i><span>'+results[i].name+'</span></a></li>')
+                    }
                 }
             }
         }
